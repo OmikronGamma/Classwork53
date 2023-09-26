08c6acb6d0c1da544f27e6b1ab56f604e9641279
Yeti <screamerip@gmail.com>
2023-09-26 23:53:11 +0500

    vet model 7

--- catalogue/models.py
+++ catalogue/models.py
@@ -3,30 +3,10 @@ from django.db import models
 # Create your models here.
 
 
-# class PetName(models.Model):
-#     pet_name = models.CharField(max_length=100)
-#
-#     def __str__(self):
-#         return self.pet_name
-
-
-# class PetBreed(models.Model):
-#     pet_breed = models.CharField(max_length=100)
-#
-#     def __str__(self):
-#         return self.pet_breed
-
-
-# class PetAge(models.Model):
-#     pet_age = models.IntegerField()
-#
-#     def __str__(self):
-#         return self.pet_age
-
-
 class PetIllness(models.Model):
+    choice = (('Light', 'Light'), ('Mild', 'Mild'), ('Severe', 'Severe'))
     illness_name = models.CharField(max_length=100)
-    illness_stage = models.IntegerField()
+    illness_stage = models.CharField(max_length=100, choices=choice)
 
     def __str__(self):
         return self.illness_name
@@ -42,12 +22,19 @@ class Treatment(models.Model):
         return self.drug_title
 
 
+
+class Doctors(models.Model):
+    doc_first_name = models.CharField(max_length=100)
+    doc_last_name = models.CharField(max_length=100)
+
+
 class VetClinic(models.Model):
     pet_name = models.CharField(max_length=100)
     pet_breed = models.CharField(max_length=100)
     pet_age = models.IntegerField()
     pet_illness = models.ForeignKey(to=PetIllness, on_delete=models.SET_NULL, null=True)
     pet_treatment = models.ManyToManyField(to=Treatment)
+    assigned_doctor = models.ForeignKey(to=Doctors, on_delete=models.SET_NULL, null=True)
 
     def __str__(self):
         return self.pet_name
