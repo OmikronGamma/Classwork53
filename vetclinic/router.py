83e9581977dd303786278086c5bf4f8330e2e1dc
Yeti <screamerip@gmail.com>
2023-10-06 00:22:14 +0500

    changes to routing

--- vetclinic/router.py
+++ vetclinic/router.py
@@ -1,40 +1,54 @@
-# routers/vetclinic.py
+# # routers/vetclinic.py
+#
+# class VetClinicRouter:
+#     """
+#     A router to control all database operations on models in the
+#     vetclinic application.
+#     """
+#     def db_for_read(self, model, **hints):
+#         """
+#         Attempts to read vetclinic models go to vetclinic db.
+#         """
+#         if model._meta.app_label == 'vetclinic':
+#             return 'vetclinic'
+#         return None
+#
+#     def db_for_write(self, model, **hints):
+#         """
+#         Attempts to write vetclinic models go to vetclinic db.
+#         """
+#         if model._meta.app_label == 'vetclinic':
+#             return 'vetclinic'
+#         return None
+#
+#     def allow_relation(self, obj1, obj2, **hints):
+#         """
+#         Allow relations if a model in the vetclinic app is involved.
+#         """
+#         if obj1._meta.app_label == 'vetclinic' or \
+#            obj2._meta.app_label == 'vetclinic':
+#            return True
+#         return None
+#
+#     def allow_migrate(self, db, app_label, model_name=None, **hints):
+#         """
+#         Make sure the catalogue app only appears in the 'catalogue'
+#         database.
+#         """
+#         if app_label == 'vetclinic':
+#             return db == 'vetclinic'
+#         return None
+
 
 class VetClinicRouter:
-    """
-    A router to control all database operations on models in the
-    vetclinic application.
-    """
     def db_for_read(self, model, **hints):
-        """
-        Attempts to read vetclinic models go to vetclinic db.
-        """
-        if model._meta.app_label == 'vetclinic':
-            return 'vetclinic'
-        return None
+        return 'vetclinic'  # always read from 'vetclinic' database
 
     def db_for_write(self, model, **hints):
-        """
-        Attempts to write vetclinic models go to vetclinic db.
-        """
-        if model._meta.app_label == 'vetclinic':
-            return 'vetclinic'
-        return None
+        return 'vetclinic'  # always write to 'vetclinic' database
 
     def allow_relation(self, obj1, obj2, **hints):
-        """
-        Allow relations if a model in the vetclinic app is involved.
-        """
-        if obj1._meta.app_label == 'vetclinic' or \
-           obj2._meta.app_label == 'vetclinic':
-           return True
-        return None
+        return True  # always allow relationships
 
     def allow_migrate(self, db, app_label, model_name=None, **hints):
-        """
-        Make sure the catalogue app only appears in the 'catalogue'
-        database.
-        """
-        if app_label == 'vetclinic':
-            return db == 'vetclinic'
-        return None
+        return db == 'vetclinic'  # only allow migrations on 'vetclinic' database
